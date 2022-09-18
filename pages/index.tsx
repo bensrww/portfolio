4fd47f17de2235b74a0645d8dc7f7fbb834504e1
Ben Chong <bensrww@gmail.com>
2022-09-19 02:51:47 +0800

    add airbnb config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import { Button } from "@mui/material";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {Button} from '@mui/material';
 
-import { useState } from "react";
+import {useState} from 'react';
 
-function Header({ title }: { title: string }) {
-  return <h1>{title ? title : "Default title"}</h1>;
+function Header({title}: {title: string}) {
+  return <h1>{title ? title : 'Default title'}</h1>;
 }
 
 function HomePage() {
-  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
 
   const [likes, setLikes] = useState(0);
 
@@ -23,7 +23,7 @@ function HomePage() {
     <div>
       <Header title="Develop. Preview. Ship. " />
       <ul>
-        {names.map((name) => (
+        {names.map(name => (
           <li key={name}>{name}</li>
         ))}
       </ul>
