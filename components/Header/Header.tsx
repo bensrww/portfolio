4c656711417fcb4ce586d5e6abd26377e99cad63
Ben Chong <bensrww@gmail.com>
2022-11-04 20:42:51 +0800

    add elements to header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,32 @@
+import FiveGSharpIcon from '@mui/icons-material/FiveGSharp';
+import {Link} from '@mui/material';
+
 import styles from './Header.module.scss';
 
 const Header = () => {
-  return <header className={styles.container}>
-    
-  </header>
-}
+  return (
+    <header className={styles.container}>
+      <div className={styles.brandIcon}>
+        <FiveGSharpIcon fontSize="inherit" />
+      </div>
+      <nav id={styles.nav}>
+        <div className={styles.navContainer}>
+          <Link underline="none" href="/">
+            Home
+          </Link>
+          <Link underline="none" href="/">
+            About
+          </Link>
+          <Link underline="none" href="/">
+            Projects
+          </Link>
+          <Link underline="none" href="/">
+            Contact
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
