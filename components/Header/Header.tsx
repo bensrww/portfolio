7011b092e0362316cc85730e1410996a0d5c5970
Ben Chong <bensrww@gmail.com>
2022-11-06 04:50:13 +0800

    add some home page elements

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,25 +6,27 @@ import styles from './Header.module.scss';
 const Header = () => {
   return (
     <header className={styles.container}>
-      <div className={styles.brandIcon}>
-        <FiveGSharpIcon color='primary' fontSize="inherit" />
-      </div>
-      <nav id={styles.nav}>
-        <div className={styles.navContainer}>
-          <Link underline="none" href="/">
-            Home
-          </Link>
-          <Link underline="none" href="/">
-            About
-          </Link>
-          <Link underline="none" href="/">
-            Projects
-          </Link>
-          <Link underline="none" href="/">
-            Contact
-          </Link>
+      <div className={styles.row}>
+        <div className={styles.brandIcon}>
+          <FiveGSharpIcon color="primary" fontSize="inherit" />
         </div>
-      </nav>
+        <nav id={styles.nav}>
+          <div className={styles.navContainer}>
+            <Link underline="none" href="/">
+              Home
+            </Link>
+            <Link underline="none" href="/">
+              About
+            </Link>
+            <Link underline="none" href="/">
+              Projects
+            </Link>
+            <Link underline="none" href="/">
+              Contact
+            </Link>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import {Button, Stack} from '@mui/material';
 
-import {useState} from 'react';
+import styles from './index.module.scss';
 
 const Header = ({title}: {title: string}) => {
   return <h1>{title.length > 0 ? title : 'Default title'}</h1>;
@@ -15,14 +15,17 @@ const HomePage = () => {
   return (
     <>
       <SectionContainer>
-        <Stack maxWidth={700}>
-          <h1>Hello World</h1>
-          <p>
+        <Stack spacing="130px" maxWidth={700}>
+          <h1 className={styles.title}>Hello World</h1>
+          <p className={styles.selfIntro}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             incididunt ut labore et dolore magna aliqua.
           </p>
-          <Button>Contact Me</Button>
+          <Button variant="contained" className={styles.ctaButton}>
+            Contact Me
+          </Button>
         </Stack>
+        <img className={styles.heroImage} src="/images/heroImage.svg" width={950} alt="hero" />
       </SectionContainer>
       <span>Other sections</span>
     </>
