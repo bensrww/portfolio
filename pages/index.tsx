97ed743810f4a48847880fb2f56f445eafe34387
Ben Chong <bensrww@gmail.com>
2022-11-07 00:16:34 +0800

    fix SectionContainer color and z-index

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,24 +7,35 @@ import styles from './index.module.scss';
 
 const HomePage = () => {
   const {t} = useTranslation('index');
+
+  const sections: JSX.Element[] = [
+    <Stack key={1} direction="row" position="relative">
+      <Stack spacing="130px" maxWidth={700}>
+        <h1 className={styles.title}>{t('title')}</h1>
+        <p className={styles.selfIntro}>{t('selfIntro')}</p>
+        <Button variant="contained" className={styles.ctaButton}>
+          Contact Me
+        </Button>
+      </Stack>
+      <img className={styles.heroImage} src="/images/heroImage.svg" alt="hero" />
+    </Stack>,
+    <span key={2}>Other sections</span>,
+  ];
+
   return (
     <>
-      <SectionContainer>
-        <Stack direction="row" position="relative">
-          <Stack spacing="130px" maxWidth={700}>
-            <h1 className={styles.title}>{t('title')}</h1>
-            <p className={styles.selfIntro}>{t('selfIntro')}</p>
-            <Button variant="contained" className={styles.ctaButton}>
-              Contact Me
-            </Button>
-          </Stack>
-          <img className={styles.heroImage} src="/images/heroImage.svg" alt="hero" />
-        </Stack>
-      </SectionContainer>
-
-      <SectionContainer isAlternate>
-        <span>Other sections</span>
-      </SectionContainer>
+      {sections.map((section, index) => {
+        return (
+          // eslint-disable-next-line react/no-array-index-key
+          <SectionContainer
+            className={styles.sectionContainer}
+            isAlternate={Boolean(index % 2)}
+            key={`section_${index}`}
+          >
+            {section}
+          </SectionContainer>
+        );
+      })}
     </>
   );
 };
