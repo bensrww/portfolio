cb84ee076968f26a9ce1691dcb60caecf8682f27
Ben Chong <bensrww@gmail.com>
2022-11-12 19:05:10 +0800

    remove unnecessary files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import {useTranslation} from 'next-i18next';
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
-import SectionContainer from '@components/SectionContainer';
 import {Button, Stack} from '@mui/material';
 
 import styles from './index.module.scss';
@@ -8,36 +7,7 @@ import styles from './index.module.scss';
 const HomePage = () => {
   const {t} = useTranslation('index');
 
-  const sections: JSX.Element[] = [
-    <Stack key={1} direction="row" position="relative">
-      <Stack spacing="130px" maxWidth={700}>
-        <h1 className={styles.title}>{t('title')}</h1>
-        <p className={styles.selfIntro}>{t('selfIntro')}</p>
-        <Button variant="contained" className={styles.ctaButton}>
-          Contact Me
-        </Button>
-      </Stack>
-      <img className={styles.heroImage} src="/images/heroImage.svg" alt="hero" />
-    </Stack>,
-    <span key={2}>Other sections</span>,
-  ];
-
-  return (
-    <>
-      {sections.map((section, index) => {
-        return (
-          <SectionContainer
-            className={styles.sectionContainer}
-            isAlternate={Boolean(index % 2)}
-            // eslint-disable-next-line react/no-array-index-key
-            key={`section_${index}`}
-          >
-            {section}
-          </SectionContainer>
-        );
-      })}
-    </>
-  );
+  return <h1>{t('title')}</h1>;
 };
 
 export const getStaticProps = async ({locale}: any) => {
